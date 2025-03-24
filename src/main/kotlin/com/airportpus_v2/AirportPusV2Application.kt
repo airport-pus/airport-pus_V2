91ad0ec9f25f1e4f551a3feefa3af5c60a0400f3
GSB0203 <gimseungbin0203@gmail.com>
2025-03-24 10:28:34 +0900

    feat(api): AirportPusV2Application
    환경 변수 사용을 위해 추가하였습니다.

--- src/main/kotlin/com/airportpus_v2/AirportPusV2Application.kt
+++ src/main/kotlin/com/airportpus_v2/AirportPusV2Application.kt
@@ -1,9 +1,12 @@
 package com.airportpus_v2
 
+import com.airportpus_v2.common.config.ApiProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApiProperties::class)
 class AirportPusV2Application
 
 fun main(args: Array<String>) {
